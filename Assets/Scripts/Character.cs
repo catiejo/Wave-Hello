be1a5132066b56ef321cb14b338a03da57371d8b
Florent D'Halluin <florent.dhalluin@gmail.com>
2017-01-20 21:31:39 +0100

    Add character scene and basic scale animation.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public AnimationCurve scaleCurve;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = scaleCurve.Evaluate(Time.time % 1) * Vector3.one;
+	}
+}
