443e9f461f8e49d8ae0bff83121be004a57e84b0
Florent D'Halluin <florent.dhalluin@gmail.com>
2017-01-20 22:01:38 +0100

    Add basic EventManager and soundTest scene.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void JoinBandAction(string friendType);
+    public static event JoinBandAction OnJoinBand;
+
+    public delegate void LeaveBandAction(string friendType);
+    public static event LeaveBandAction OnLeaveBand;
+
+    public void JoinBand(string friendType)
+    {
+        if (EventManager.OnJoinBand != null)
+        {
+            EventManager.OnJoinBand(friendType);
+        }
+    }
+
+    public void LeaveBand(string friendType)
+    {
+        if (EventManager.OnLeaveBand != null)
+        {
+            EventManager.OnLeaveBand(friendType);
+        }
+    }
+}
