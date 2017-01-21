3f902d94df8ce9de1effd3c53efaf7105b01c540
Florent D'Halluin <florent.dhalluin@gmail.com>
2017-01-21 03:05:12 +0100

    Tweak character scale, animations

--- Assets/Scripts/UniformScaleAnimation.cs
+++ Assets/Scripts/UniformScaleAnimation.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class UniformScaleAnimation : MonoBehaviour {
 
 	public AnimationCurve scaleCurve;
-    public static float globalBPM = 120;
+    public static float globalBPM = 30;
 
     // Use this for initialization
     void Start () {
---
+++ Assets/Scripts/XYScaleAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XYScaleAnimation : MonoBehaviour {
+
+	public AnimationCurve xScaleCurve;
+    public AnimationCurve yScaleCurve;
+    public static float globalBPM = 30;
+
+	// Update is called once per frame
+	void Update () {
+        var bpmFactor = 60.0f / globalBPM;
+        var xScale = xScaleCurve.Evaluate((Time.time % bpmFactor) / bpmFactor);
+        var yScale = yScaleCurve.Evaluate((Time.time % bpmFactor) / bpmFactor);
+        this.transform.localScale = new Vector3(xScale, yScale, 1);
+	}
+}
