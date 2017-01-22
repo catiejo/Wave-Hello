fe81317d3d1b5e07f9c4680d879f3086be3dfb9d
Florent D'Halluin <florent.dhalluin@gmail.com>
2017-01-22 07:23:44 +0100

    Update world

---
+++ Assets/Scripts/StickOnTerrain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class StickOnTerrain : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up * 200, Vector3.down, out hit, 400))
+        {
+            transform.position = hit.point;
+        }
+    }
+}
